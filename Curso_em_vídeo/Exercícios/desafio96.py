80ed2eb03c44d2258f34748485d941bf11a416ac
Lucas_Stanski <stanskilucas390@gmail.com>
2023-06-09 17:18:44 -0300

    novo

---
+++ Curso_em_vídeo/Exercícios/desafio96.py
@@ -0,0 +1,7 @@
+def medir(a,b):
+    s=a*b
+    print(f"A área do terrno é {s}m².")
+print("Controle de terrenos")
+largura=float(input("Largura (m): "))
+comprimento=float(input("Comprimento (m): "))
+medir(largura,comprimento)
