60369c211d8e6a077805dcd8f2d81d14bbba8f10
Lucas_Stanski <stanskilucas390@gmail.com>
2023-06-16 15:12:31 -0300

    novo

--- Curso_em_vídeo/Exercícios/desafio103.py
+++ Curso_em_vídeo/Exercícios/desafio103.py
@@ -1,5 +1,14 @@
 def ficha(Jog="<desconhecido>",gols=0):
     print(f"O Jogador {Jog} fez {gols} gol(s) no campeonato.")
+
+
 n=str(input("Nome do jogador: "))
-g=int(input("Número de gols: "))
-if g.is
+g=str(input("Número de gols: "))
+if g.isnumeric():
+    g=int(g)
+else:
+    g=0
+if n.strip()=="":
+    ficha(gols=g)
+else:
+    ficha(n,g) 
